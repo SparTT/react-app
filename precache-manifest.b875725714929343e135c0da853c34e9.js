673ca483c69f0707db87ad0492f764d1e604415a
SparTT <leoguedes3011@gmail.com>
2020-07-02 12:56:53 -0300

    Updates

---
+++ precache-manifest.b875725714929343e135c0da853c34e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddaf3481826048480ecf67f1dcda5093",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "8dff438d7e20d6f47f60",
+    "url": "/react-app/static/css/main.e0b19565.chunk.css"
+  },
+  {
+    "revision": "78773ed5197c2c52774b",
+    "url": "/react-app/static/js/2.082ff867.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.082ff867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dff438d7e20d6f47f60",
+    "url": "/react-app/static/js/main.a18fcec0.chunk.js"
+  },
+  {
+    "revision": "b418e1a6a01bb0f923a9",
+    "url": "/react-app/static/js/runtime-main.14961523.js"
+  }
+]);
